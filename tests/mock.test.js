ea637198acf1f9406be36603f8d3f9e4957c695b
Paulo Gonçalves <paulorochag@hotmail.com>
2021-01-07 12:09:05 -0300

    tests: implementar teste usando mockResolvedValue()

--- tests/mock.test.js
+++ tests/mock.test.js
@@ -2,10 +2,10 @@ const src = require('../src')
 const axios = require('axios')
 
 describe('Mock', () => {
-  afterEach(() => jest.restoreAllMocks())
+  beforeEach(() => jest.clearAllMocks())
 
   /*
-  Mockando um módulo
+  Mockando um módulo manualmente
   Manual Mocks - https://jestjs.io/docs/en/manual-mocks
   */
   it('Manual Mock - Mockando um módulo manualmente', async () => {
@@ -19,6 +19,37 @@ describe('Mock', () => {
     })
     expect(user.quantidade).toBe(5)
     expect(user.usuarios[0].email).toBe('teste@qa.com')
+  })
+
+  /*
+  Mockando um módulo com mockResolvedValue()
+  Mocking Modules - https://jestjs.io/docs/en/mock-functions#mocking-modules
+  */
+  it('mockResolvedValue() - Mockando um módulo com mockResolvedValue()', async () => {
+    jest.mock('axios')
+
+    axios.get.mockResolvedValue({
+      data: {
+        quantidade: 102030,
+        usuarios: [
+          {
+            nome: 'Fulano da Silva',
+            email: 'fulano.teste@qa.com'
+          }
+        ]
+      }
+    })
+
+    const user = await src.getUserByEmail('fulano.teste@qa.com')
+
+    expect(axios.get).toHaveBeenCalledTimes(1)
+    expect(axios.get).toHaveBeenCalledWith('https://serverest.dev/usuarios', {
+      params: {
+        email: 'fulano.teste@qa.com'
+      }
+    })
+    expect(user.quantidade).toBe(102030)
+    expect(user.usuarios[0].email).toBe('fulano.teste@qa.com')
   })
 
   /*
