049325f7511ed93ab709a05874f52604ef36b93e
Paulo Gonçalves <paulorochag@hotmail.com>
2021-01-07 10:16:02 -0300

    test(mock): tornar valor de e-mail do GET dinâmico

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,13 +1,13 @@
 // mock de módulo utilizado em mock.test.js e método getUserByEmail
 
 module.exports = {
-  get: jest.fn(() => Promise.resolve({
+  get: jest.fn((rota, { params }) => Promise.resolve({
     data: {
       quantidade: 5,
       usuarios: [
         {
           nome: 'Fulano',
-          email: 'teste@qa.com'
+          email: params.email
         }
       ]
     }
--- tests/mock.test.js
+++ tests/mock.test.js
@@ -9,16 +9,16 @@ describe('mock', () => {
   Manual Mocks - https://jestjs.io/docs/en/manual-mocks
   */
   it('Should return user correctly when getUserByEmail is called', async () => {
-    const email = 'teste@qa.com'
-    const user = await src.getUserByEmail(email)
+    const user = await src.getUserByEmail('teste@qa.com')
 
     expect(mockedAxios.get).toHaveBeenCalledTimes(1)
     expect(mockedAxios.get).toHaveBeenCalledWith('https://serverest.dev/usuarios', {
       params: {
-        email
+        email: 'teste@qa.com'
       }
     })
     expect(user.quantidade).toBe(5)
+    expect(user.usuarios[0].email).toBe('teste@qa.com')
   })
 
   /*
